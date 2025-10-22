2970f00f256c2cbdda9ad7e5fdd32e56cc47912b
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-10-22 10:01:21 +0000

    great start, but still cant move forward properly

--- Assets/Scripts/Scooter/MyScooter.cs
+++ Assets/Scripts/Scooter/MyScooter.cs
@@ -2,48 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BNG;
+using UnityEngine.Audio;
+
+// grip: requirement for the scooter to move;
+// but how to do both hand grabs?
+
+// right trigger: brake (StopScooter())
+// left trigger: speed (MoveScooterForward())
+
+// either Button1 or Button2: turn on/off the light
+// either Button2 or Button1: honk sound
+
+//  virtual void OnGrab(Grabber grabber):  Item has been grabbed by a Grabber
+//  virtual void OnRelease(): Has been dropped from the Grabber
+//  virtual void OnGrip(float gripValue): Amount of Grip(0-1)
+//  virtual void OnTrigger(float triggerValue):  Amount of Trigger being held down on the grabbed items controller
+//  virtual void OnTriggerDown(): Fires if trigger was pressed down on this controller this frame
+//  virtual void OnTriggerUp(): Fires if trigger was released on this controller this frame
+//  virtual void OnButton1(): Button 1 is being held down this frame but not last Oculus : Button 1 = "A" if held in Right controller
+//  virtual void OnButton1Down(): Button 1 Pressed down this frame Oculus : Button 1 = "A" if held in Right controller
+//  virtual void OnButton1Up(): Button 1 Released this frame Oculus : Button 1 = "A" if held in Right controller
+//  virtual void OnButton2(): Button 2 is being held down this frame but not last Oculus : Button 2 = "B" if held in Right controller
+//  virtual void OnButton2Down(): Button 2 Pressed down this frame Oculus : Button 2 = "B" if held in Right controller
+//  virtual void OnButton2Up(): Button 2 Released this frame Oculus : Button 2 = "B" if held in Right controller
+//  Protected Attributes
+//  Grabbable grab
+//  Grabber thisGrabber
+//  Grabber thisGrabber
 
 public class MyScooter : GrabbableEvents
 {
-    public float Force;
+    public float Force = 10f;
     public ParticleSystem driftingEffect;
-    AudioSource audioSource;
-
-    // grip: requirement for the scooter to move;
-    // but how to do both hand grabs?
-
-    // left trigger: brake (StopScooter())
-    // right trigger: speed (MoveScooterForward())
-
-    // left trigger: turn on/off the light
-    // right trigger: honk sound
-
-
-    //  virtual void OnGrab(Grabber grabber):  Item has been grabbed by a Grabber
-    //  virtual void OnRelease(): Has been dropped from the Grabber
-    //  virtual void OnGrip(float gripValue): Amount of Grip(0-1)
-    //  virtual void OnTrigger(float triggerValue):  Amount of Trigger being held down on the grabbed items controller
-    //  virtual void OnTriggerDown(): Fires if trigger was pressed down on this controller this frame
-    //  virtual void OnTriggerUp(): Fires if trigger was released on this controller this frame
-    //  virtual void OnButton1(): Button 1 is being held down this frame but not last Oculus : Button 1 = "A" if held in Right controller
-    //  virtual void OnButton1Down(): Button 1 Pressed down this frame Oculus : Button 1 = "A" if held in Right controller
-    //  virtual void OnButton1Up(): Button 1 Released this frame Oculus : Button 1 = "A" if held in Right controller
-    //  virtual void OnButton2(): Button 2 is being held down this frame but not last Oculus : Button 2 = "B" if held in Right controller
-    //  virtual void OnButton2Down(): Button 2 Pressed down this frame Oculus : Button 2 = "B" if held in Right controller
-    //  virtual void OnButton2Up(): Button 2 Released this frame Oculus : Button 2 = "B" if held in Right controller
-    //  Protected Attributes
-    //  Grabbable grab
-    //  Grabber thisGrabber
-    //  Grabber thisGrabber
-
+    public AudioSource honkSound;
+    public AudioClip honkAudioClip;
+    public Light drivingLight;
 
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        honkSound = GetComponentInChildren<AudioSource>();
+        drivingLight = GetComponentInChildren<Light>();
+        driftingEffect = GetComponentInChildren<ParticleSystem>();
     }
-    
-    // Update or FixedUpdate
-    void FixedUpdate()
+
+    // Update or FixedUpdate?
+    void Update()
     {
         if (grab != null && grab.BeingHeld)
         {
@@ -67,44 +70,46 @@ public class MyScooter : GrabbableEvents
         base.OnTrigger(triggerValue);
     }
 
-    //Vector3 moveDirection;
+    //  Vector3 moveDirection;
 
     void MoveScooterForward(float triggerValue)
     {
         //moveDirection = transform.forward * Force;
-        transform.position = transform.forward * Force * triggerValue * Time.deltaTime;
+        transform.position += transform.forward * Force * Time.deltaTime;
 
-        if (!audioSource.isPlaying)
-        {
-            audioSource.Play();
-        }
-
-        if (driftingEffect != null && !driftingEffect.isPlaying)
-        {
-            driftingEffect.Play();
-        }
+        //if (driftingEffect != null && !driftingEffect.isPlaying)
+        //{
+        //    driftingEffect.Play();
+        //}
     }
     public override void OnRelease()
     {
         StopScooter();
     }
 
-    public override void OnTriggerUp()
-    {
-        StopScooter();
-        base.OnTriggerUp();
-    }
+    //public override void OnTriggerUp()
+    //{
+    //    StopScooter();
+    //    base.OnTriggerUp();
+    //}
 
     void StopScooter()
     {
-        if (audioSource.isPlaying)
+        //if (driftingEffect != null && driftingEffect.isPlaying)
+        //{
+        //    driftingEffect.Stop();
+        //}
+        transform.position += Vector3.zero;
+    }
+    void Honk()
+    {
+        if (honkSound)
         {
-            audioSource.Stop();
+            honkSound.PlayOneShot(honkAudioClip);
         }
-
-        if (driftingEffect != null && driftingEffect.isPlaying)
+        else
         {
-            driftingEffect.Stop();
+            Debug.Log("Can't honk rn");
         }
     }
 }
