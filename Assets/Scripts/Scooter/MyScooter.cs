0b2c574def09c820be6f1ea7fe53b8e1dc3c16b3
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-10-22 19:04:40 +0000

    physics logic minor fix, with light and honking sound added

--- Assets/Scripts/Scooter/MyScooter.cs
+++ Assets/Scripts/Scooter/MyScooter.cs
@@ -2,64 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BNG;
-using UnityEngine.Audio;
 
-// grip: requirement for the scooter to move;
-// but how to do both hand grabs?
+// left trigger: brake (StopScooter())
+// right trigger: speed (MoveScooterForward())
 
-// right trigger: brake (StopScooter())
-// left trigger: speed (MoveScooterForward())
-
-// either Button1 or Button2: turn on/off the light
-// either Button2 or Button1: honk sound
-
-//  virtual void OnGrab(Grabber grabber):  Item has been grabbed by a Grabber
-//  virtual void OnRelease(): Has been dropped from the Grabber
-//  virtual void OnGrip(float gripValue): Amount of Grip(0-1)
-//  virtual void OnTrigger(float triggerValue):  Amount of Trigger being held down on the grabbed items controller
-//  virtual void OnTriggerDown(): Fires if trigger was pressed down on this controller this frame
-//  virtual void OnTriggerUp(): Fires if trigger was released on this controller this frame
-//  virtual void OnButton1(): Button 1 is being held down this frame but not last Oculus : Button 1 = "A" if held in Right controller
-//  virtual void OnButton1Down(): Button 1 Pressed down this frame Oculus : Button 1 = "A" if held in Right controller
-//  virtual void OnButton1Up(): Button 1 Released this frame Oculus : Button 1 = "A" if held in Right controller
-//  virtual void OnButton2(): Button 2 is being held down this frame but not last Oculus : Button 2 = "B" if held in Right controller
-//  virtual void OnButton2Down(): Button 2 Pressed down this frame Oculus : Button 2 = "B" if held in Right controller
-//  virtual void OnButton2Up(): Button 2 Released this frame Oculus : Button 2 = "B" if held in Right controller
-//  Protected Attributes
-//  Grabbable grab
-//  Grabber thisGrabber
-//  Grabber thisGrabber
+// "A" to toggle the light
+// "X" to honk
 
 public class MyScooter : GrabbableEvents
 {
-    public float Force = 10f;
+    public float force = 10f;
     public ParticleSystem driftingEffect;
+    Rigidbody rb;
+
+    Grabber leftHand, rightHand;
+
     public AudioSource honkSound;
     public AudioClip honkAudioClip;
+    
     public Light drivingLight;
 
     void Start()
     {
+        driftingEffect = GetComponentInChildren<ParticleSystem>();
+        rb = GetComponent<Rigidbody>();
         honkSound = GetComponentInChildren<AudioSource>();
         drivingLight = GetComponentInChildren<Light>();
-        driftingEffect = GetComponentInChildren<ParticleSystem>();
+        drivingLight.enabled = false;
     }
 
     // Update or FixedUpdate?
-    void Update()
+    void FixedUpdate()
     {
-        if (grab != null && grab.BeingHeld)
-        {
-            //
-        }
-    }
-    public override void OnGrab(Grabber grabber)
-    {
-
-    }
-    public override void OnTrigger(float triggerValue)
-    {
-        if (triggerValue > 0.25f)
+        // if (grab != null && grab.BeingHeld)
+        // {
+        //     //
+        // }
+        if (leftHand != null && rightHand != null)
         {
             MoveScooterForward(triggerValue);
         }
@@ -67,40 +46,75 @@ public class MyScooter : GrabbableEvents
         {
             StopScooter();
         }
-        base.OnTrigger(triggerValue);
+    }
+    
+    public override void OnGrab(Grabber grabber)
+    {
+        if(grabber.HandSide == ControllerHand.Left) 
+        {
+            leftHand = grabber;
+        }
+        else if(grabber.HandSide == ControllerHand.Right)
+        {
+            rightHand = grabber;
+        }
     }
 
-    //  Vector3 moveDirection;
+    public override void OnTrigger(float triggerValue)
+    {
+        if(leftHand && rightHand)
+        {
+            MoveScooterForward(triggerValue);
+        }
+        else
+        {
+            StopScooter();
+        }
+    }
 
     void MoveScooterForward(float triggerValue)
     {
-        //moveDirection = transform.forward * Force;
-        transform.position += transform.forward * Force * Time.deltaTime;
-
-        //if (driftingEffect != null && !driftingEffect.isPlaying)
-        //{
-        //    driftingEffect.Play();
-        //}
+        // transform.position += transform.forward * force * Time.deltaTime;
+        rb.AddForce(transform.forward * force, ForceMode.Acceleration);
+        // if (driftingEffect != null && !driftingEffect.isPlaying)
+        // {
+        //     driftingEffect.Play();
+        // }
     }
+
     public override void OnRelease()
     {
+        leftHand = null;
+        rightHand = null;
         StopScooter();
     }
 
-    //public override void OnTriggerUp()
-    //{
-    //    StopScooter();
-    //    base.OnTriggerUp();
-    //}
-
     void StopScooter()
     {
-        //if (driftingEffect != null && driftingEffect.isPlaying)
-        //{
-        //    driftingEffect.Stop();
-        //}
-        transform.position += Vector3.zero;
+        rb.velocity = Vector3.zero;
+        // if (driftingEffect != null && driftingEffect.isPlaying)
+        // {
+        //     driftingEffect.Stop();
+        // }
+        // transform.position += Vector3.zero;
     }
+
+    public override void OnButton1Down()
+    {
+        if (thisGrabber.HandSide == ControllerHand.Left)
+        {
+            // left hand press "X" to honk
+            Debug.Log("Left hand pressed X");
+            Honk();
+        }
+        else if (thisGrabber.HandSide == ControllerHand.Right)
+        {
+            // right hand press "A" to toggle the driving light
+            Debug.Log("Right hand pressed A");
+            ToggleLight();
+        }
+    }
+	
     void Honk()
     {
         if (honkSound)
@@ -109,7 +123,15 @@ public class MyScooter : GrabbableEvents
         }
         else
         {
-            Debug.Log("Can't honk rn");
+            Debug.Log("Can't honk rn!");
+        }
+    }
+
+    void ToggleLight() 
+    {
+        if(drivingLight)
+        {
+            drivingLight.enabled = !drivingLight.enabled;
         }
     }
 }
